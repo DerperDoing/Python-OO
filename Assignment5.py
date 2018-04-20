d0c08d4383e26885277ecee2d8d736d8de10b5f4
Mayank Bagchi <37003722+DerperDoing@users.noreply.github.com>
2018-04-20 22:49:57 +0530

    Create Assignment5.py

---
+++ Assignment5.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self,studentid=1,studentname="ljdvmxlck", qualifyingexammarks=46.5, residentialstatus='j', yearofengg=3, branchname="CS"):
+        print("Student Id: ",studentid)
+        print("Student Name: ", studentname)
+        print("Qualifying Exam Marks: ", qualifyingexammarks)
+        print("Residential Status: ", residentialstatus)
+        print("Current Year of Engineering: ",yearofengg)
+        print("Branch Name: ",branchname)
+
+deets=Student()
+
+#deets=Student("PUT VALUES ACCORDINGLY FOR 2nd PART OF THIS ASSIGNMENT :D")
